a544810133dbb2dfb1babcbf605e37721ef245ed
yeop <yp1222@koast.net>
2023-05-25 00:42:45 +0900

    [feat] chapter 7 - thymeleaf CRUD
    블로그 화면 구성

---
+++ src/main/java/me/yeop/springbootdeveloper/controller/BlogViewController.java
@@ -0,0 +1,54 @@
+package me.yeop.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.yeop.springbootdeveloper.domain.Article;
+import me.yeop.springbootdeveloper.dto.ArticleListViewResponse;
+import me.yeop.springbootdeveloper.dto.ArticleResponse;
+import me.yeop.springbootdeveloper.dto.ArticleViewResponse;
+import me.yeop.springbootdeveloper.service.BlogService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if(id == null) {
+            // id 없으면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            // id 없으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
+    }
+}
